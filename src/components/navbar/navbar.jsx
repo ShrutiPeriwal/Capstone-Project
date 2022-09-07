2475c2e77b409017ffc92f587e5ecc3bb522b1c4
Shruti <shrutiperiwal21@gmail.com>
2022-09-07 18:13:38 +0530

    capstone

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,54 +1,77 @@
-import React from 'react';
-import './navbar.css';
-import { FiLogIn } from 'react-icons/fi';
-import { BsLinkedin } from 'react-icons/bs';
-import {AiOutlineShoppingCart } from 'react-icons/ai';
+import React, { useState } from "react";
+import "./navbar.css";
+import { FiLogIn } from "react-icons/fi";
+import { BsLinkedin } from "react-icons/bs";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { NavLink } from 'react-router-dom';
 
-const navbar = () => {
+const Navbar = () => {
+  const [showMediaIcons, setShowMediaIcons] = useState(false);
   return (
     <>
-        <nav className="navbar-main">
-            <div className="navbar-logo">
-                <h2>
-                    <span> W</span>ebsite
-                </h2>
-            </div>
+      <nav className="navbar-main">
+        <div className="navbar-logo">
+          <h2>
+            <span> W</span>ebsite
+          </h2>
+        </div>
 
-            <div className="navbar-menu">
-                <ul>
-                    <li>
-                        <a href="#">Hats</a>
-                    </li>
-                    <li>
-                        <a href="#">Jackets</a>
-                    </li>
-                    <li>
-                        <a href="#">Shoes</a>
-                    </li>
-                    <li>
-                        <a href="#">Tops</a>
-                    </li>
-                </ul>
-            </div>
+        <div
+          className={
+            showMediaIcons ? "navbar-menu mobile-navbar-menu" : "navbar-menu"
+          }
+        >
+          <ul>
+            <li>
+              <NavLink to="/Hats">Hats</NavLink>
+            </li>
+            <li>
+              <NavLink to="/Jackets">Jackets</NavLink>
+            </li>
+            <li>
+              <NavLink to="/Shoes">Shoes</NavLink>
+            </li>
+            <li>
+              <NavLink to="/Tops">Tops</NavLink>
+            </li>
+          </ul>
+        </div>
 
-            <div className="navbar-media">
-                <ul className="media">
-                     <li>
-                        <a href="#"><FiLogIn /></a>
-                    </li> 
-                     <li>
-                        <a href="#"><AiOutlineShoppingCart /></a>
-                    </li>
-                    <li>
-                        <a href="https://www.linkedin.com/in/shruti-periwal-11400b213/" target="_blank
+        <div className="navbar-media">
+          <ul className="media">
+            <li>
+              <a href="#">
+                <FiLogIn className="login" />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <AiOutlineShoppingCart className="cart" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/shruti-periwal-11400b213/"
+                target="_blank
                         
-                        "><BsLinkedin /></a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </>
-  )
-}
+                        "
+              >
+                <BsLinkedin className="linklin" />
+              </a>
+            </li>
+          </ul>
 
-export default navbar;
+          {/* hamburget menu start  */}
+          <div className="hamburger-menu">
+            <a href="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
+              <GiHamburgerMenu />
+            </a>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
