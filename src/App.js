959834cda7c7853359c470dd53dcf6ab8cf263a3
Shruti <shrutiperiwal21@gmail.com>
2022-09-08 21:57:24 +0530

    slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/navbar/navbar';
+import Slider from './components/Slider/slider';
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Slider />
     </div>
   )
 }
