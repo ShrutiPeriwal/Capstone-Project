57851f6a5c0693855e6aa9cf1923706a9554b5cf
Shruti <shrutiperiwal21@gmail.com>
2022-09-09 13:14:18 +0530

    card

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/navbar/navbar';
 import ControlledCarousel from './components/Slider/slider';
@@ -9,7 +10,7 @@ const App = () => {
     <div>
       <Navbar />
       <ControlledCarousel />
-      <h3>Top Selling Products</h3>
+      <h3 className="heading">Top Selling Products</h3>
       <CardComponent />
     </div>
   )
--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,17 +1,16 @@
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
+import './card.css';
 
 function CardComponent() {
   return (
-    <CardGroup>
-    
-      <Card>
+    <CardGroup className="CardGroup">
+        <Card className="Card">
         <Card.Img variant="jackets" src="https://images.pexels.com/photos/1070970/pexels-photo-1070970.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
         <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
+          <Card.Title className="title">Jacket</Card.Title>
+          <Card.Text className="text">
+          Full Sleeve Solid Women Casual Jacket
           </Card.Text>
         </Card.Body>
         <Card.Footer>
@@ -21,10 +20,9 @@ function CardComponent() {
       <Card>
         <Card.Img variant="heels" src="https://images.pexels.com/photos/1445696/pexels-photo-1445696.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
         <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This card has supporting text below as a natural lead-in to
-            additional content.{' '}
+          <Card.Title className="title">Heels</Card.Title>
+          <Card.Text className="text">
+          Women Pink Heels Sandal{' '}
           </Card.Text>
         </Card.Body>
         <Card.Footer>
@@ -34,11 +32,9 @@ function CardComponent() {
       <Card>
         <Card.Img variant="tops" src="https://images.pexels.com/photos/1449664/pexels-photo-1449664.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
         <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This card has even longer content than the
-            first to show that equal height action.
+          <Card.Title className="title">Tops</Card.Title>
+          <Card.Text className="text">
+          Casual Regular Sleeves Women Brown Top
           </Card.Text>
         </Card.Body>
         <Card.Footer>
