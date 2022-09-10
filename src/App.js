2c96051f3c6569b425f0ac0f8307a3b7d879b266
Shruti <shrutiperiwal21@gmail.com>
2022-09-10 20:03:36 +0530

    slider

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/navbar/navbar';
-import SignIn from './components/signin/signin'
+import SignIn from './components/signin/signin';
+import SignUp from './components/signup/signup';
 import Footer from './components/footer/footer';
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      
-     
       <Footer /> 
       <SignIn />
+      <SignUp />
     </div>
   )
 }
