1fa255288944abd9492f7a8f955a5e1c735f1641
Shruti <shrutiperiwal21@gmail.com>
2022-09-12 11:25:55 +0530

    card

---
+++ src/category/hats/hatdata.jsx
@@ -0,0 +1,27 @@
+export const HatData = [
+    {
+      id: "1",
+      Name: "Nike Hat",
+      Price: 500,
+      path: "https://images.pexels.com/photos/1124465/pexels-photo-1124465.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: "2",
+      Name: "Causal Hat",
+      Price: 300,
+      path: "https://images.pexels.com/photos/954254/pexels-photo-954254.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: "3",
+      Name: "LA Hat",
+      Price: 450,
+      path: "https://images.pexels.com/photos/844867/pexels-photo-844867.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: "4",
+      Name: "LA Hat",
+      Price: 450,
+      path: "https://images.pexels.com/photos/844867/pexels-photo-844867.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+  ];
+  
