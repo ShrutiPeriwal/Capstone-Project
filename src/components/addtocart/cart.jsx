87c770e87cb7d37e69371e56e90a50e3be46616e
Shruti <shrutiperiwal21@gmail.com>
2022-09-13 23:45:05 +0530

    cart

--- src/components/addtocart/cart.jsx
+++ src/components/addtocart/cart.jsx
@@ -13,38 +13,68 @@ const Cart = () => {
     emptyCart,
   } = useCart();
 
-  if(isEmpty) return <h1 className="text-center">Your Cart is Empty</h1>
+  if (isEmpty) return <h1 className="text-center">Your Cart is Empty</h1>;
   return (
     <section className="py-4 container">
-        <div className="row justify-content-center">
-            <div className="col-12">
-                <h5> Cart ({ totalUniqueItems }) total Items: ({ totalItems }) </h5>
+      <div className="row justify-content-center">
+        <div className="col-12">
+          <h5>
+            Cart ({totalUniqueItems}) total Items: ({totalItems})
+          </h5>
 
-                <table className="table table-light table-hover m-0">
-                <tbody>
-                    {items.map((item, index) => {
-                        return (
-                        <tr key={index}>
-                            <td>
-                                <img src={item.path} style={{height: '6rem'}}/>
-                            </td>
-                            <td>{item.Name}</td>
-                            <td>{item.Price}</td>
-                            <td>Quantity ({item.Quantity})</td>
-                            <td>
-                                <button className="btn btn-info ms-2">-</button>
-                                <button className="btn btn-info ms-2">+</button>
-                                <button className="btn btn-danger ms-2">Remove Item</button>
-                            </td>
-                        </tr>
-                        )
-                    })}
-                    </tbody>
-                </table>
-            </div>
+          <table className="table table-light table-hover m-0">
+            <tbody>
+              {items.map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <td>
+                      <img src={item.path} style={{ height: "6rem" }} />
+                    </td>
+                    <td>{item.Name}</td>
+                    <td>{item.Price}</td>
+                    <td>Quantity ({item.Quantity})</td>
+                    <td>
+                      <button
+                        className="btn btn-info ms-2"
+                        onClick={() =>
+                          updateItemQuantity(item.id, item.Quantity - 1)
+                        }
+                      >
+                        -
+                      </button>
+                      <button
+                        className="btn btn-info ms-2"
+                        onClick={() =>
+                          updateItemQuantity(item.id, item.Quantity + 1)
+                        }
+                      >
+                        +
+                      </button>
+                      <button
+                        className="btn btn-danger ms-2"
+                        onClick={() => removeItem(item.id)}
+                      >
+                        Remove Item
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
         </div>
+        <div className="col-auto ms-auto">
+              <h2>TotalPrice: $ {cartTotal}</h2>
+        </div>
+        <div className="col-auto">
+              <button className="btn btn-danger m-2"
+              onClick={()=> emptyCart()}
+              >Clear Cart</button>
+              <button className="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
     </section>
-  )
+  );
 };
 
 export default Cart;
