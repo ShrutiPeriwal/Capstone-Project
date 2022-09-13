06ddd71b3205dd655b7ba221149851f12f7f0f73
Shruti <shrutiperiwal21@gmail.com>
2022-09-13 22:31:53 +0530

    cart

--- src/components/addtocart/cart.jsx
+++ src/components/addtocart/cart.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
+import { CartProvider, useCart } from "react-use-cart";
+import { TopData } from "../../category/tops/topdata";
 
-const cart = () => {
+function Cart() {
+  const { addItem } = useCart();
   return (
-    <div>cart</div>
-  )
+    <>
+      {TopData.map((p) => (
+        <div key={p.id}>
+          <button onClick={() => addItem(p)}>Add To Cart</button>
+        </div>
+      ))}
+    </>
+  );
 }
 
-export default cart
+function Page() {
+    const {
+      isEmpty,
+      totalUniqueItems,
+      items,
+      updateItemQuantity,
+      removeItem,
+    } = useCart();
+
+    if (isEmpty) return <p>Your cart is empty</p>;
+
+  return (
+    <>
+      <h1>Cart ({totalUniqueItems})</h1>
+
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            {item.quantity} x {item.name} &mdash;
+            <button
+              onClick={() => updateItemQuantity(item.id, item.quantity - 1)}
+            >
+              -
+            </button>
+            <button
+              onClick={() => updateItemQuantity(item.id, item.quantity + 1)}
+            >
+              +
+            </button>
+            <button onClick={() => removeItem(item.id)}>&times;</button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default Cart;
