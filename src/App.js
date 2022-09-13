444cb4d6c3634ead8fab8607e646e2cddb70066a
Shruti <shrutiperiwal21@gmail.com>
2022-09-13 23:23:00 +0530

    cart

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
-import React from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import Home from './components/home/home';
-import Navigation from './components/navbar/navbar.jsx';
-import Footer from './components/footer/footer.jsx'
-import SignIn from './components/signin/signin.jsx';
-import SignUp from './components/signup/signup';
-import Hat from './category/hats/hat';
-import Jacket from './category/jackets/jacket';
-import Shoe from './category/shoes/shoe';
-import Top from './category/tops/top';
-import AddToCart from './components/addtocart/cart';
+import Home from "./components/home/home";
+import Navigation from "./components/navbar/navbar.jsx";
+import Footer from "./components/footer/footer.jsx";
+import SignIn from "./components/signin/signin.jsx";
+import SignUp from "./components/signup/signup";
+import Hat from "./category/hats/hat";
+import Jacket from "./category/jackets/jacket";
+import Shoe from "./category/shoes/shoe";
+import Top from "./category/tops/top";
+import AddToCart from "./components/addtocart/cart";
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { CartProvider } from 'react-use-cart';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CartProvider } from "react-use-cart";
 
 const App = () => {
   return (
-    <BrowserRouter>
     <CartProvider>
-    <Navigation />
-      <Routes>
-        <Route  path='/' element={<Home />}/>
-        <Route path='/signin' element={<SignIn />}/>
-        <Route path='/signup' element={<SignUp />}/>
-        <Route path='/hat' element={<Hat />}/>
-        <Route path='/jacket' element={<Jacket />}/>
-        <Route path='/shoe' element={<Shoe />}/>
-        <Route path='/top' element={<Top />}/>
-        <Route path='/addtocart' element={<AddToCart />}/>
-      </Routes>
-      <Footer />
-      </CartProvider>
-    </BrowserRouter>
-  )
-}
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/hat" element={<Hat />} />
+          <Route path="/jacket" element={<Jacket />} />
+          <Route path="/shoe" element={<Shoe />} />
+          <Route path="/top" element={<Top />} />
+          <Route path="/addtocart" element={<AddToCart />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </CartProvider>
+  );
+};
 
 export default App;
--- src/components/addtocart/cart.jsx
+++ src/components/addtocart/cart.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
-import { useCart } from 'react-use-cart';
+import React from "react";
+import { useCart } from "react-use-cart";
 
-const cart = () => {
+const Cart = () => {
+  const {
+    isEmpty,
+    totalUniqueItems,
+    items,
+    totalItems,
+    cartTotal,
+    updateItemQuantity,
+    removeItem,
+    emptyCart,
+  } = useCart();
+
+  if(isEmpty) return <h1 className="text-center">Your Cart is Empty</h1>
   return (
-    <div>cart</div>
-  )
-}
+    <section className="py-4 container">
+        <div className="row justify-content-center">
+            <div className="col-12">
+                <h5> Cart ({ totalUniqueItems }) total Items: ({ totalItems }) </h5>
 
-export default cart
+                <table className="table table-light table-hover m-0">
+                <tbody>
+                    {items.map((item, index) => {
+                        return (
+                        <tr key={index}>
+                            <td>
+                                <img src={item.path} style={{height: '6rem'}}/>
+                            </td>
+                            <td>{item.Name}</td>
+                            <td>{item.Price}</td>
+                            <td>Quantity ({item.Quantity})</td>
+                            <td>
+                                <button className="btn btn-info ms-2">-</button>
+                                <button className="btn btn-info ms-2">+</button>
+                                <button className="btn btn-danger ms-2">Remove Item</button>
+                            </td>
+                        </tr>
+                        )
+                    })}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </section>
+  )
+};
+
+export default Cart;
