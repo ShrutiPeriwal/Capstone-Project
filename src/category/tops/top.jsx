7452f558b7ef23dd1bb5f9f57bd054cc687cff17
Shruti <shrutiperiwal21@gmail.com>
2022-09-14 18:51:37 +0530

    cart

--- src/category/tops/top.jsx
+++ src/category/tops/top.jsx
@@ -2,15 +2,31 @@ import * as React from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
+import { useCart } from "react-use-cart";
 import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
+import { Button, Box} from "@mui/material";
 import Typography from "@mui/material/Typography";
 import { TopData } from "./topdata";
+import Modal from '@mui/material/Modal';
 
-import { useCart } from "react-use-cart";
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
-export default function TopCard() {
+export default function TopCard({props}) {
   const { addItem } = useCart();
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   return (
     <>
@@ -28,7 +44,7 @@ export default function TopCard() {
               display: "inline-grid",
             }}
           >
-            <CardMedia component="img" height="140" image={props.path} />
+            <CardMedia component="img" height="140" image={props.path} onClick={handleOpen} />
             <CardContent>
               <Typography gutterBottom variant="h5" component="div">
                 Name : {props.Name}
@@ -40,8 +56,7 @@ export default function TopCard() {
             <CardActions>
               <Button variant="outlined" style={{ marginLeft: "30px" }}
               onClick={() => 
-        
-              addItem(props.item)}
+              addItem(props)}
               >
                 Add To Cart
               </Button>
@@ -49,6 +64,23 @@ export default function TopCard() {
           </Card>
         );
       })}
+
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
     </>
   );
 }
